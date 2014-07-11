e0b39c88c13ce5330f0b7fa07026a5a3b3f2eabf
Shruthi Venkatesh <Svenkatesh@uchicago.edu>
2014-07-11 18:23:07 -0400

    Working on metro var

---
+++ Housing55/merge_beale.py
@@ -0,0 +1,20 @@
+import pandas as pd 
+
+# Beale data into df 
+y = 1991 
+beale = pd.read_csv('M:/Senior Living/Data/PSID Data/All_weights/newbeale8511.csv')
+
+# Yearly data
+fpath = 'M:/Senior Living/Data/PSID Data/All_weights/' + str(y) + '.csv'
+vfpath = 'M:/Senior Living/Data/PSID Data/All_weights/agecohort_vars.csv'
+famintnum = (pd.read_csv(vfpath).groupby('year').get_group(y)).loc[0, 'famintnum']
+ydf = pd.read_csv(fpath)
+
+# Isolate yearly obs from Beale df
+yBeale = pd.DataFrame(beale.loc[beale['CBV2'] == y])
+
+# Merge yearly obs to yearly data by fam id
+
+
+
+# Outsheet 
