66f642d23a83bcc39e125e3a714b3ebcbf4f82e6
Shruthi Venkatesh <Svenkatesh@uchicago.edu>
2014-07-15 18:16:41 -0400

    metro

--- Housing55/merge_beale.py
+++ Housing55/merge_beale.py
@@ -3,14 +3,14 @@ import pandas as pd
 
 # Beale data into df 
 y = 1991 
-#bealedata = 'M:/Senior Living/Data/PSID Data/All_weights/newbeale8511.csv'
-bealedata = '/Users/ShruthiVenkatesh/Documents/newbeale8511/newbeale8511.csv'
+bealedata = 'M:/Senior Living/Data/PSID Data/Beale Urbanicity/newbeale8511.csv'
+#bealedata = '/Users/ShruthiVenkatesh/Documents/newbeale8511/newbeale8511.csv'
 beale = pd.read_csv(bealedata)
 
 # Yearly data
-#fpath = 'M:/Senior Living/Data/PSID Data/All_weights/' + str(y) + '.csv'
-fpath = ''
-vfpath = 'M:/Senior Living/Data/PSID Data/All_weights/agecohort_vars.csv'
+fpath = 'M:/Senior Living/Data/PSID Data/years/' + str(y) + '.csv'
+#fpath = '/users/ShruthiVenkatesh/Documents/Senior Living Project/Data/' + str(y) + '.csv'
+vfpath = 'M:/Senior Living/Data/PSID Data/agecohort_vars.csv'
 famintnum = (pd.read_csv(vfpath).groupby('year').get_group(y)).loc[0, 'famintnum']
 ydf = pd.read_csv(fpath)
 
