719b9bb2fe3ff2ebf7f4aac41b942b7b6608d73c
Shruthi Venkatesh <Svenkatesh@uchicago.edu>
2014-07-25 16:43:20 -0400

    Finished usepsid class

--- Housing55/usePSID.py
+++ Housing55/usePSID.py
@@ -22,7 +22,7 @@ class usePSID:
 		varlist = self.getVars()
 		return varlist.loc[0, label]
 
-	# Isolate head obs
+	# Isolate household head obs
 	def headsOnly(self):
 		data = self.df 
 		seqnum_var = self.getVarname('seqnum')
@@ -51,49 +51,70 @@ class usePSID:
 		dfnew = data.loc[(data[agevar] >= lower) &  (data[agevar] <= upper)]
 		agecat = pd.cut(dfnew[agevar], 
 			bins = nbins, 
-			labels = False, 
-			retbins = True) 
-		df_agecat = pd.DataFrame(agecat[0], columns = ['agecat'])
+			labels = False) 
+		df_agecat = pd.DataFrame(agecat, columns = ['agecat'])
 		return pd.merge(dfnew, df_agecat, 
 			left_index = True, 
 			right_index = True, 
 			right_on = ['agecat'])
-		
+
+	def getCodes(self, varlabel):
+		if varlabel == 'htenure': return {8: "Neither owns or rents", 9: "NA; Refused", 5: "Rents", 1: "Owns"}
+		if varlabel == 'hstructure': return {1: "One-family house", 2: "Two-family house", 3: "Apartment/housing project",
+							4: "Mobile homes", 6: "Townhouse", 7: "Other", 9: "NA"}
+		if varlabel == 'seniorh': return {1: 'Yes', 5: 'No', 3: "Don't know", 8: "Don't know", 9:'NA', 0: "inap"}
+		if varlabel == 't_seniorh': return {1: 'Retirement community', 2: 'Senior citizen housing', 
+							3: 'Nursing home', 4:'Home for the aged', 5: 'Adult foster care', 6: 'Assisted living', 
+							8: "Don't know", 9: "NA", 0: "inap"}
+		else: return 'Varlabel not found'
+
 	# Compute weighted share of a group by values of varlab
 	def computeShare(self, dfgr, varlab, wtlab): 
 		output = {}
 		data = self.df
 		select_var = self.getVarname(varlab)
 		wt_var = self.getVarname(wtlab)
-		for g in dfgr.groupby(select_var): 
-			(group, info) = g
-			output[str(group)+'_'+str(self.year)] = np.true_divide(info[wt_var].sum(), dfgr[wt_var].sum())
+		codes = self.getCodes(varlab)
+		if select_var in dfgr.columns: 
+			for g in dfgr.groupby(select_var): 
+				(group, info) = g
+				output[str(self.year)+'_'+codes[group]] = np.true_divide(info[wt_var].sum(), dfgr[wt_var].sum())
 		return output 
 
+	# Merge the Beale Urbanicity variable to a yearly df object
+	def mergeBeale(self, fbeale): 
+		beale = pd.read_csv(fbeale)
+		y_beale = pd.DataFrame(beale.loc[beale['CBV2'] == self.year])
+		famintnum = self.getVarname('famintnum')
+		data = (self.df).dropna(subset=[famintnum])
+		yBeale.rename(columns = {'CBV3': famintnum}, inplace = True)
+		return pd.merge(ydf, yBeale, how = 'outer')
 
 # Testing functions in cohortShare 
-
-years = [1991, 1992]
-nbins = range(50, 105, 5)
+years = range(1991, 1997) + range(1997, 2013, 2)
+nbins = range(50, 100, 5)
+varlabel = "t_seniorh"
+f_output = "M:/Senior Living/Data/PSID Data/Age Profiles/" + varlabel + ".csv"
 vname = "M:/Senior Living/Data/PSID Data/Agecohort_vars.csv"
 
-def testSuite(y, nbins): 
-	output = {i:{} for i in range(0, len(nbins)-1)}
+def testSuite(y, nbins, varlabel): 
+	output = {i:{} for i in nbins[0:len(nbins)-1]}
 	fname = 'M:/Senior Living/Data/PSID Data/Years/' + str(y) + '.csv'
 	obj = usePSID(y, fname, vname)
 	# Create an age indicator 
-	df_age = obj.mergeAgeCohort(100, 50, nbins)
+	df_age = obj.mergeAgeCohort(max(nbins), min(nbins), nbins)
 	# Group by the age indicator 
 	df_age_gr = df_age.groupby('agecat')
 	# For each group, compute share of housing tenure/ type/ etc
 	for g in df_age_gr: 
 		(agegr, info) = g
-		output[agegr].update(obj.computeShare(info, 'htenure', 'indweight'))
+		output[nbins[int(agegr)]].update(obj.computeShare(info, varlabel, 'indweight'))
 	return output 
 
 if __name__ == "__main__": 
-	output = {i:{} for i in range(0, len(nbins)-1)}
+	output = {i:{} for i in nbins[0:len(nbins)-1]}
 	for y in years: 
-		yearlyinfo = testSuite(y, nbins)
+		yearlyinfo = testSuite(y, nbins, varlabel)
 		[output[i].update(yearlyinfo[i]) for i in yearlyinfo]
-	print output 
+		print y
+	(pd.DataFrame(output)).to_csv(f_output)
