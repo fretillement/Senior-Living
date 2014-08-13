679b92bb4411fa57543af2dad5a05f6c7e4737fd
Shruthi Venkatesh <Svenkatesh@uchicago.edu>
2014-08-13 17:59:00 -0400

    Expanding panel data

--- Housing55/get_panel.py
+++ Housing55/get_panel.py
@@ -9,7 +9,7 @@ Key variables: house type, senior housing status,
 housing tenure, type of senior housing
 '''
 # Edit filepaths below 
-vfpath = 'M:/Senior Living/Data/PSID Data/agecohort_vars.csv'
+vfpath = 'M:/Senior Living/Code/Senior-Living/Psid_clean/agecohort_vars.csv'
 dfpath = 'M:/Senior Living/Data/PSID Data/J176012.csv'
 ofpath = 'M:/Senior Living/Data/PSID Data/Panel/elderly_panel.csv'
 
@@ -20,6 +20,9 @@ years = range(1991, 1998) + range(1999, 2012, 2)
 # Find varlabels across time 
 vardict = {}
 allvars = pd.DataFrame.to_dict(pd.read_csv(vfpath, index_col='year'))
+
+print allvars['age']
+
 #varlist = list(set(itertools.chain(*(allvars.values))))
 for k in allvars: 
 	for l in allvars[k]:
@@ -29,7 +32,29 @@ for k in allvars:
 master = pd.read_csv(dfpath, usecols=vardict.keys())
 
 # Keep only obs past the age of lower
-output = master.loc[(master['ER34104'] >= lower) \
+output = master.loc[(master['ER30004'] >= lower) \
+				| (master['ER30023'] >= lower) \
+				| (master['ER30046'] >= lower) \
+				| (master['ER30070'] >= lower) \
+				| (master['ER30094'] >= lower) \
+				| (master['ER30120'] >= lower) \
+				| (master['ER30141'] >= lower) \
+				| (master['ER30163'] >= lower) \
+				| (master['ER30191'] >= lower) \
+				| (master['ER30220'] >= lower) \
+				| (master['ER30249'] >= lower) \
+				| (master['ER30286'] >= lower) \
+				| (master['ER30316'] >= lower) \
+				| (master['ER30346'] >= lower) \
+				| (master['ER30376'] >= lower) \
+				| (master['ER30402'] >= lower) \
+				| (master['ER30432'] >= lower) \
+				| (master['ER30466'] >= lower) \
+				| (master['ER30501'] >= lower) \
+				| (master['ER30538'] >= lower) \
+				| (master['ER30573'] >= lower) \
+				| (master['ER30609'] >= lower) \
+				| (master['ER30645'] >= lower) \
 				| (master['ER30692'] >= lower) \
 				| (master['ER30736'] >= lower) \
 				| (master['ER30809'] >= lower) \
@@ -43,7 +68,7 @@ output = master.loc[(master['ER34104'] >= lower) \
 				| (master['ER33804'] >= lower) \
 				| (master['ER33904'] >= lower) \
 				| (master['ER34004'] >= lower) \
-				| (master['ER34104'] >= lower)]
+				| (master['ER34104'] >= lower) ]
 
 # Rename column names
 for v in output: 
@@ -55,7 +80,7 @@ output['unique_pid'] = output.personnum2011.map(str)+ "_" + output.id19682011.ma
 # Reorder column names
 header = output.columns.tolist()
 cols = ['unique_pid']+ ['id19682011']+ ['personnum2011']+\
-		[x for x in header if 'htenure' in x]+ \
+		[x for x in header if 'htenure ' in x]+ \
 		[x for x in header if 'hstructure' in x]+ \
 		[x for x in header if 'seniorh' in x and 't_seniorh' not in x]+ \
 		[x for x in header if 't_seniorh' in x]+ \
