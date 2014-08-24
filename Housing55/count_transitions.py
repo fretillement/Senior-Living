df9085f15bf7bd4bc53e1b396d15d0a722820365
Shruthi Venkatesh <svenkatesh@uchicago.edu>
2014-08-24 01:23:39 -0400

    Finished filling in missing age values to transitions

--- Housing55/count_transitions.py
+++ Housing55/count_transitions.py
@@ -2,9 +2,9 @@ import pandas as pd
 import os
 
 ''' 
-This script counts and characterizes the housing 
-transitions of the elderly in PSID file J170612 
+This script counts the housing transitions of the elderly in PSID file J170612 
 '''
+
 # Set filepaths
 #out = "M:/Senior Living/Data/PSID Data/Panel/transitions.csv"
 out = "/Users/ShruthiVenkatesh/Desktop/transitions.csv"
@@ -13,12 +13,28 @@ paneldata ="/users/ShruthiVenkatesh/Desktop/elderly_panel.csv"
 #paneldata = "M:/Senior Living/Data/PSID Data/Panel/elderly_panel.csv"
 vardata = "M:/Senior Living/Code/Senior-Living/Psid_clean/agecohort_vars.csv"
 
+
+# Label PSID housing structure variables 
+namesdict = {1.0: 'Single-family house', 
+			 2.0: 'Duplex/ 2-family house', 
+			 3.0: 'Multifamily', 
+			 4.0: 'Mobile Home/ trailer', 
+			 5.0: 'Condo', 
+			 6.0: 'Townhouse', 
+			 7.0: 'Other', 
+			 8.0: "Don't know", 
+			 9.0: "Refused" 
+			 }
+
+
 # Extract info about elderly only 
 #execfile(panel_code)
+
 
 # Get years
 years = map(str, range(1975, 1998) + range(1999, 2012, 2))
 missing = map(str, [1973, 1974, 1982])
+
 
 # Read panel and varlabel data  
 df = pd.read_csv(paneldata)
@@ -32,8 +48,9 @@ def totalTransitions(df):
 	df['num_transitions'] = transitions.apply(lambda x: x.nunique()-1, axis=1)
 	return df
 
+
 # Generate dummy vars for each type of transition (1-9)
-def countTransitions(df): 
+def countTransitions(df, namesdict): 
 	# Takes a df with housing structure vars and person id
 	# Returns df with dummies for each type of transition entered
 
@@ -48,17 +65,7 @@ def countTransitions(df):
 	# Pivot the observations 
 	piv = st.pivot(index='index', columns='hstructure', values='unique_pid')
 
-	# Rename columns
-	namesdict = {1.0: 'Single-family house', 
-				 2.0: 'Duplex/ 2-family house', 
-				 3.0: 'Multifamily', 
-				 4.0: 'Mobile Home/ trailer', 
-				 5.0: 'Condo', 
-				 6.0: 'Townhouse', 
-				 7.0: 'Other', 
-				 8.0: "Don't know", 
-				 9.0: "Refused" 
-				 }
+	# Rename the columns
 	piv = piv.rename(columns=namesdict)
 
 	# Convert the info in pivoted df to dummy vars
@@ -96,44 +103,49 @@ def ageLookup(row, age_df):
 	# Return the row
 	return agevalue
 
+# The next two functions fill in missing age observations
+# based on a single age provided and the year variable
 def fillAges(group): 
+	# Print the person's id
+	#print group['unique_pid'].get_values()[0]
 	group.reset_index(inplace=True)
-	g = group['age']
-	first = g[g>0].first_valid_index()
-	last = g[g>0].last_valid_index()
-	l = len(g.index)
-	if (first > 0) and (last+1 < l): print ["Odd solutions", group['unique_pid']]
-	'''
-	if (last+1== l and first == 0):
-		group['age2'] = calcAges(group, g, first, last, l, True)
+
+	first = group['age'].loc[group['age']>0].first_valid_index()
+	last = group['age'].loc[group['age']>0].last_valid_index()	
+	l = len(group['age'].index)
+	
+	# Check for missing observations. If none missing return same age values.
+	if (first > 0) and (last+1 < l): 
+		group['age2'] = len(group.index)*[999]
 		return group
-	if (first + last + 1 >l):
-		group['age2'] = calcAges(group, g, first, last, l, False)
+	if (first + last + 1 == l):
+		group['age2'] = group['age']
 		return group
-	'''
+	if (first + last + 1 > l):
+		# If missing values, call algo to fill in values
+		group['age2'] = calcAges(group, first, last, l)
+		return group
 
+def calcAges(gr, first, last, l): 	
+	# Calculate age based on the first and last complete age observation
+	o = gr['age'].loc[gr['age']>0].tolist()
+	tempage = int(gr['age'][first])
 
-def calcAges(gr, g, first, last, l, condition): 	
-	o = g[g>0].tolist()
-	tempage = g.ix[first, 'year']
-	if condition: 
-		return pd.DataFrame(gr['year'])
-	if not condition:
-		while (first > 0):
-			thisyr = gr.ix[first ,'year']
-			prevyr = gr.ix[first-1, 'year']
-			tempage = tempage - (thisyr-prevyr)
-			if tempage == 999: tempage = 0 + tempage
-			o = [tempage]+o
-			first -= 1
-		gr.set_index('year', inplace=True, drop=False)
-		d = pd.DataFrame.from_dic(dict(zip(gr['year'].tolist(), o)), orient='index')
-		return d
-	else : 
-		return "Error"
+	while (first > 0):
+		# While there are missing values, iterate over the empty values 
+		thisyr = gr.ix[first ,'year']
+		prevyr = gr.ix[first-1, 'year']
 
+		# Calculate age by subtracting difference between years from current age 
+		tempage = tempage - (thisyr-prevyr)
 
+		# If the age is 999, fill in all values as 999 
+		if tempage == 999: tempage = 0 + tempage
+		o = [tempage]+o
+		first -= 1
 
+	# Return a list of filled in ages 
+	return o
 
 
 if __name__ == "__main__": 
@@ -141,33 +153,6 @@ if __name__ == "__main__":
 	#fn = lambda x: ageLookup(x, age_df)
 	#tcounts['age'] = tcounts.apply(fn, axis=1)
 	#tcounts.set_index('unique_pid', inplace=True)
-	#print tcounts.head(30)
-	#tcounts.to_csv(out)
-	#fillAges(out)
-
-	df1 = pd.read_csv(out, usecols=['age', 'unique_pid', 'year'])
-	'''
-	gr = df1.groupby('unique_pid').get_group(13)	
-	gr.reset_index(inplace=True)
-	g = gr['age']
-	first = g[g>0].first_valid_index()
-	last = g[g>0].last_valid_index()
-	l = len(g.index)	
-	
-	o = g[g>0].tolist()
-	if (first + last + 1 >l):
-		tempage = g.ix[first, 'year']
-		while (first > 0):
-			thisyr = gr.ix[first ,'year']
-			prevyr = gr.ix[first-1, 'year']
-			tempage = tempage - (thisyr-prevyr)
-			o = [tempage]+o
-			first -= 1
-	gr.set_index('year', inplace=True, drop=False)
-	d = dict(zip(gr['year'].tolist(), o))
-	d2 = pd.DataFrame.from_dict(d, orient='index')
-
-	'''	
-	df1.groupby('unique_pid').apply(fillAges)
-	#df3 = df2.apply(fillAges)
-	#print df1.head(30)
+	#df_output = tcounts.groupby('unique_pid').apply(fillAges)
+	#df_output.drop('index', axis=1, inplace=True)
+	#df_output.to_csv('/users/ShruthiVenkatesh/Desktop/transitions2.csv', index=False, columns=['unique_pid', 'age', 'age2']+namesdict.values())
